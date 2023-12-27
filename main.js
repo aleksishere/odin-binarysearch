f967b082e7577ff80c4fb12108ddb5d388f918e0
Aleks <24192100+aleksishere@users.noreply.github.com>
2023-12-27 17:43:01 +0100

    Finished exercise

--- main.js
+++ main.js
@@ -106,6 +106,147 @@ class Tree {
       }
     }
   }
+
+  find(node) {
+    let currentNode = this.root
+    while (true) {
+      if (currentNode === null) return false;
+      if (currentNode.data === node) return true;
+      if (node < currentNode.data) {
+        currentNode = currentNode.left
+      }
+      if (node > currentNode.data) {
+        currentNode = currentNode.right
+      }
+    }
+  }
+
+  levelOrder(callback) {
+    if (this.root === null) {
+      return;
+    }
+    let queue = [this.root];
+    let result = []
+    let currentNode;
+    while (queue.length > 0) {
+      currentNode = queue[0];
+      queue.shift();
+      if (callback) {
+        callback(currentNode);
+      } else {
+        result.push(currentNode.data);
+      }
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    if (!callback) {
+      return result;
+    }
+  }
+
+  inOrder(callback) {
+    let result = [];
+    function recursive(node) {
+      if (node === null) return;
+      recursive(node.left)
+      if (callback) {
+        callback(node);
+      } else {
+        result.push(node.data);
+      }
+      recursive(node.right)
+    }
+    recursive(this.root);
+    if (!callback) {
+      return result;
+    }
+  }
+
+  preOrder(callback) {
+    let result = [];
+    function recursive(node) {
+      if (node === null) {
+        return;
+      }
+      if (callback) {
+        callback(node)
+      } else {
+        result.push(node.data)
+      }
+      recursive(node.left)
+      recursive(node.right)
+    }
+    recursive(this.root)
+    if (!callback) {
+      return result;
+    }
+  }
+
+  height() {
+    function recursive(node) {
+      if (node === null) return 0;
+      let left = recursive(node.left);
+      let right = recursive(node.right);
+      return Math.max(left, right) + 1;
+    }
+    return recursive(this.root);
+  }
+
+  postOrder(callback) {
+    let result = [];
+    function recursive(node) {
+      if (node === null) return;
+      recursive(node.left)
+      recursive(node.right)
+      if (callback) {
+        callback(node)
+      } else {
+        result.push(node.data);
+      }
+    }
+    recursive(this.root);
+    if (!callback) return result;
+  }
+
+  depth(value) {
+    let current = this.root;
+    let depth = 0;
+    while (current !== null) {
+      if (current.data === value) return depth;
+      if (current.data > value) {
+        current = current.left;
+        depth++;
+      } else if (current.data < value) {
+        current = current.right;
+        depth++;
+      }
+    }
+    console.error("Value not found")
+    return false;
+  }
+
+  isBalanced() {
+    function recursive(node) {
+      if (node === null) return 0;
+      let left = recursive(node.left);
+      if (left === -1) return -1;
+      let right = recursive(node.right);
+      if (right === -1) return -1;
+      if (Math.abs(left - right > 1)) {
+        return -1;
+      } else return Math.max(left, right) + 1;
+    }
+    return recursive(this.root) !== -1;
+  }
+
+  rebalance() {
+    let newArray = this.inOrder();
+    this.root = this.buildTree(newArray);
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -121,8 +262,28 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-let tree = new Tree([4, 3, 2, 4, 5, 5, 6]);
-tree.insert(54);
-prettyPrint(tree.root);
-tree.delete(4);
-prettyPrint(tree.root);
+function testBST() {
+  let numbers = [];
+  for (let i = 0; i < 15; i++) {
+    let number = Math.floor(Math.random() * (100 - 1) + 1);
+    numbers.push(number);
+  }
+  let tree = new Tree(numbers);
+  prettyPrint(tree.root)
+  console.log(tree.isBalanced())
+  console.log(tree.preOrder())
+  console.log(tree.postOrder())
+  console.log(tree.inOrder())
+  for (let i = 0; i < 5; i++) {
+    let number = Math.floor(Math.random() * (1000 - 100) + 100);
+    tree.insert(number);
+  }
+  console.log(tree.isBalanced())
+  prettyPrint(tree.root)
+  tree.rebalance()
+  console.log(tree.isBalanced())
+  prettyPrint(tree.root)
+  console.log(tree.preOrder())
+  console.log(tree.postOrder())
+  console.log(tree.inOrder())
+}
